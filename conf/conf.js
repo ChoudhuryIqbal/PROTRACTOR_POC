ff48859bcf43c569ea9ac80850ec0ca504dff93e
Choudhury Iqbal <ChoudhuryIqbal@users.noreply.github.com>
2020-11-10 15:50:23 -0500

    reporter added.

--- conf/conf.js
+++ conf/conf.js
@@ -1,22 +1,30 @@
 // An example configuration file.
+var HtmlReporter = require("protractor-beautiful-reporter");
 exports.config = {
-    directConnect: true,
-  
-    // Capabilities to be passed to the webdriver instance.
-    capabilities: {
-      'browserName': 'chrome'
-    },
-  
-    // Framework to use. Jasmine is recommended.
-    framework: 'jasmine',
-  
-    // Spec patterns are relative to the current working directory when
-    // protractor is called.
-    specs: ['..//testcases//pomTest.js'],
-  
-    // Options to be passed to Jasmine.
-    jasmineNodeOpts: {
-      defaultTimeoutInterval: 30000
-    }
-  };
-  
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    browserName: "chrome",
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: "jasmine",
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ["..//testcases//MouseOver.js"],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000,
+  },
+  onPrepare: function () {
+    // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+    jasmine.getEnv().addReporter(
+      new HtmlReporter({
+        baseDirectory: "Reports/screenshots",
+      }).getJasmine2Reporter()
+    );
+  },
+};
